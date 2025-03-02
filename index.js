6ac5cb1fbdf099d3bcb63ba5bed4361673deb4af
ColonelAtlas <tidiane.ndiouck19@gmail.com>
2025-03-02 11:16:17 +0100

    Add files via upload
    boum

---
+++ index.js
@@ -0,0 +1,12 @@
+// Show sidebar
+function showSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'flex';
+}
+
+// Hide sidebar
+function hideSidebar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'none';
+}
+

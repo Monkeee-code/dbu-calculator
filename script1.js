f1aba8b636c3fc39d97bb0c496db5f235e838cff
_Monkeee <markokuruzovic9@gmail.com>
2025-03-07 20:16:19 +0100

    Fixed Stat needed to reb (SYSTEM)

--- script1.js
+++ script1.js
@@ -112,16 +112,11 @@ function calculateGain() {
 
     // Step 6: Calcul du nombre de stats nÃ©cessaires pour rebirth
     function getRebStats() {
-        if (rebirths <= 8) {
-            if (rebirths == 0) {
-                let reb0 = 1000000;
-                return reb0.toLocaleString('en-US');
-            }
-            let reb = rebirths * 1000000;
+        if (rebirths <= 9) {
+            let reb = (rebirths * 1000000) + 1000000;
             return reb.toLocaleString('en-US');
-        } else if (rebirths > 8) {
-            const m = 2000000;
-            const stats = (m * rebirths - 8000000);
+        } else if (rebirths > 9) {
+            const stats = 10000000 + ((9-rebirths) * 2000000);
             return stats.toLocaleString('en-US');
         }
         // const m = 2000000

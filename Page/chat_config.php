60da9c4e4bc0a38e2ef7376409411450f0d5ae26
_Monkeee <114259817+Monkeee-code@users.noreply.github.com>
2025-06-25 13:40:36 +0200

    Update chat_config.php

--- Page/chat_config.php
+++ Page/chat_config.php
@@ -1,6 +1,6 @@
 <?php
 
 define('CLIENT_ID', "1387373756778807366");
-define('CLIENT_SECRET', "teIYgSKv1YAVZwkiTqDwYAE7Xj8Okilv");
-define('REDIRECT_URI', "http://localhost/Page/chat_callback.php");
+define('CLIENT_SECRET', "");
+define('REDIRECT_URI', "");
 define('DISCORD_API_BASE_URL', "https://discord.com/api");

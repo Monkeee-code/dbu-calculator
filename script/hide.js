b547e633d589fabe500420a99fd114110e73e114
Monkeee-code <markokuruzovic9@gmail.com>
2025-06-26 12:55:21 +0200

    Bug fixes

---
+++ script/hide.js
@@ -0,0 +1,12 @@
+const isAvailable = false
+if (!isAvailable) {
+    const hidder = document.createElement("div");
+    hidder.style.width = "100%";
+    hidder.style.height = "100%";
+    hidder.style.backgroundColor = "white";
+    hidder.style.color = "black";
+    hidder.appendChild(document.querySelector("body"));
+    setTimeout(async () => {
+        window.prompt("This page has been hidden due to developers, hidding it!\nIf you have any questions, join our discord: https://discord.gg/dAtcaSmDSs", "https://discord.gg/dAtcaSmDSs")        
+        }, 1000)
+}
--- script/script1.js
+++ script/script1.js
@@ -1,19 +1,3 @@
-// Toggle For website's elements!
-
-const isAvailable = false
-if (!isAvailable) {
-    const hidder = document.createElement("div");
-    hidder.style.width = "100%";
-    hidder.style.height = "100%";
-    hidder.style.backgroundColor = "white";
-    hidder.style.color = "black";
-    hidder.appendChild(document.querySelector("body"));
-    setTimeout(async () => {
-        window.prompt("This page has been due to developers, hidding it!\nIf you have any questions, join our discord: https://discord.gg/dAtcaSmDSs", "https://discord.gg/dAtcaSmDSs")        
-        }, 1000)
-}
-
-
 const talentsMapping = { yes: 1, no: 0 };
 
 function generateBossOptions() {
